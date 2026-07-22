58dfe33b257be90e7dccd0b17c9eaa880900134a
captainamerica2026 <captainamerica3960@gmail.com>
2026-07-22 16:51:22 +0500

    Update radar.py

--- app/ui/radar.py
+++ app/ui/radar.py
@@ -1,46 +1,87 @@
-# HashGuard Radar Engine
+# HashGuard Radar Animation Engine
 
-import time
+from kivy.uix.widget import Widget
+from kivy.graphics import (
+    Color,
+    Ellipse,
+    Line
+)
+from kivy.clock import Clock
 
 
-class RadarScanner:
+class Radar(Widget):
 
 
-    def __init__(self):
+    def __init__(self, **kwargs):
 
-        self.modules = [
-            "Battery",
-            "CPU",
-            "RAM",
-            "Storage",
-            "Thermal",
-            "Sensors"
+        super().__init__(**kwargs)
+
+        self.angle = 0
+
+
+        with self.canvas:
+
+            Color(
+                0,
+                0.8,
+                1,
+                0.4
+            )
+
+
+            self.circle = Ellipse(
+                pos=self.center,
+                size=(250,250)
+            )
+
+
+            self.radar_line = Line(
+                points=[
+                    self.center_x,
+                    self.center_y,
+                    self.center_x+100,
+                    self.center_y
+                ],
+                width=2
+            )
+
+
+        Clock.schedule_interval(
+            self.update_radar,
+            0.02
+        )
+
+
+
+    def update_radar(self, dt):
+
+        self.angle += 2
+
+
+        import math
+
+
+        x = (
+            self.center_x +
+            100 *
+            math.cos(
+                math.radians(self.angle)
+            )
+        )
+
+
+        y = (
+            self.center_y +
+            100 *
+            math.sin(
+                math.radians(self.angle)
+            )
+        )
+
+
+        self.radar_line.points = [
+            self.center_x,
+            self.center_y,
+            x,
+            y
         ]
-
-
-
-    def start(self):
-
-        print("📡 RADAR SYSTEM ONLINE")
-
-
-
-    def scan_modules(self):
-
-        results = {}
-
-        print("\nStarting deep scan...\n")
-
-
-        for module in self.modules:
-
-            print("Scanning:", module)
-
-            time.sleep(0.5)
-
-            results[module] = "PASS"
-
-
-        print("\nScan Complete")
-
-        return results
