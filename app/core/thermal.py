c416a1dceb3903f92e68a52785b68a3362c074e4
captainamerica2026 <captainamerica3960@gmail.com>
2026-07-22 16:56:50 +0500

    Create thermal.py

---
+++ app/core/thermal.py
@@ -0,0 +1,14 @@
+# HashGuard Thermal Engine
+
+
+class ThermalScanner:
+
+
+    def scan(self):
+
+        return {
+
+            "temperature": "31°C",
+            "status": "NORMAL"
+
+        }
