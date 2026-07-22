8d48466fc606db7c786938d270dbe54092c8beda
captainamerica2026 <captainamerica3960@gmail.com>
2026-07-22 17:11:44 +0500

    Create sensors.py

---
+++ app/core/sensors.py
@@ -0,0 +1,16 @@
+# HashGuard Sensor Scanner
+
+
+class SensorScanner:
+
+
+    def scan(self):
+
+        return {
+
+            "GPS": "Available",
+            "Camera": "Available",
+            "Bluetooth": "Available",
+            "Gyroscope": "Available"
+
+        }
