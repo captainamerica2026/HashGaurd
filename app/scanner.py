ba00ffc6b64214a5f30399164b7423089deb2c54
captainamerica2026 <captainamerica3960@gmail.com>
2026-07-22 16:36:10 +0500

    Create scanner.py

---
+++ app/scanner.py
@@ -0,0 +1,14 @@
+class Scanner:
+
+    def scan(self):
+
+        print("Starting scan...")
+
+        results = {
+            "battery": "GOOD",
+            "hardware": "GOOD",
+            "storage": "GOOD",
+            "thermal": "NORMAL"
+        }
+
+        return results
